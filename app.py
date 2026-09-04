04f50f93e518589967f34108dc1d316efd7b245c
Vaibhav Prakash <vaibhav.prakash147@gmail.com>
2026-09-04 07:17:57 +0530

    FEATURE FIRST COMMIT

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 def main():
     name = input("Enter your name: ")
     print(f"Hello, {name}! Welcome to my Git + GitHub Actions practice app.")
+    print(f"Did you know? Your name has {len(name)} characters.")
 
 if __name__ == "__main__":
     main()

7e2d2b8a05f1ac6f8fce1efc4919a4a231f1b238
Vaibhav Prakash <vaibhav.prakash147@gmail.com>
2026-09-11 09:58:26 +0530

    fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
-from app import get_name_length
+from app import app
 
-
-def test_get_name_length():
-    assert get_name_length("Vaibhav") == 7
+def test_home_route():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.data == b"Hello from Docker!"
